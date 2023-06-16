ba0f6baeb09d3cf008a9de0afd6a8f1033a8a790
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-15 23:32:18 -0300

    Update main.php

--- TCC/main.php
+++ TCC/main.php
@@ -1,47 +1,57 @@
 <?php
-    $includeNavbar = true;
-    if ($includeNavbar) {
-        include("navbar.php");
-    }
+$includeNavbar = true;
+if ($includeNavbar) {
+    include("navbar.php"); // Inclui a navbar
+}
+require("conn.php");
+
+// Verifica se o cookie foi definido para exibir o pop-up
+if (!isset($_COOKIE["popupShown"])) {
+    setcookie("popupShown", "true", time() + (86400 * 1), "/"); // Define o cookie por 30 dias
+    $showPopup = true;
+} else {
+    $showPopup = false;
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="CSS/main.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            <?php
-            session_start();
-            if (isset($_SESSION['login_status'])) {
-                if ($_SESSION['login_status'] === 'success') {
-                    $nome_usuario = $_SESSION['nome_usuario'];
-                    echo "showPopup('Bem-vindo, $nome_usuario!', 'success-popup');";
-                } elseif ($_SESSION['login_status'] === 'error') {
-                    echo "showPopup('Usuário e/ou senha inválidos!', '');";
-                }
-                unset($_SESSION['login_status']);
-            }
-            ?>
-
-            function showPopup(message, styleClass) {
-                var popup = $('<div></div>').addClass('popup').addClass(styleClass);
-                var popupMessage = $('<p></p>').text(message);
-
-                popup.append(popupMessage);
-
-                $('body').append(popup);
-
-                setTimeout(function() {
-                    popup.addClass('fadeOut');
-                    setTimeout(function() {
-                        popup.remove();
-                    }, 1000);
-                }, 8000);
-            }
-        });
-    </script>
+    <link rel="stylesheet" href="CRUD/main.css">
+    <title>Pop-up com Janela Modal</title>
 </head>
 <body>
+    <?php if ($showPopup) : ?>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Login efetuado com sucesso!</p>
+        </div>
+    </div>
+
+    <script>
+        // Função para exibir o pop-up
+        function showModal() {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+
+            var span = document.getElementsByClassName("close")[0];
+
+            // Função para fechar o pop-up ao clicar no botão Fechar (x)
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // Função para fechar o pop-up ao clicar fora da janela modal
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+
+        // Chamar a função showModal quando a página for carregada
+        window.onload = showModal;
+    </script>
+    <?php endif; ?>
 </body>
 </html>
